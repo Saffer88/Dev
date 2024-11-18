970462ceb1bace431d629b8b669fa5af8c4dab13
Javier Romo <ul.jromoa@gmail.com>
2024-11-18 18:15:07 -0300

    don't do the search

--- React/Buscador-de-peliculas/src/hooks/useMovies.js
+++ React/Buscador-de-peliculas/src/hooks/useMovies.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import { searchMovies } from '../services/movies.js'
 
 //const API_MOVIES_URL = `https://www.omdbapi.com/?apikey=34bda7af&S=$(inputSearch)`
@@ -8,21 +8,26 @@ export function useMovies({ inputSearch}) {
     const [movies, setMovies] = useState([])
     const [error, setError] = useState(null)
     const [loading, setLoanding] = useState(false)
+    const previusSearch = useRef({ inputSearch })
     
 
     
 
     const getMovies = async () => {
+      if(inputSearch === previusSearch.current) return
+
       try{
         setLoanding(true)
-        setError(null)   
+        setError(null)
+        previusSearch.current = inputSearch   
         const newMovies = await searchMovies({ inputSearch })
         setMovies(newMovies)
 
-      }catch(e){
+      } catch (e) {
           setError(e.message)
-      }finally{
-        setLoanding(false)
+      } finally {
+          setLoanding(false)
+
       }
     }
     
