a957eac6dbb87c5e681257c1c91277876596ad9a
Javier Romo <ul.jromoa@gmail.com>
2024-11-19 16:57:07 -0300

    add debounce and automatic search

--- React/Buscador-de-peliculas/src/hooks/useMovies.js
+++ React/Buscador-de-peliculas/src/hooks/useMovies.js
@@ -1,35 +1,42 @@
-import { useRef, useState } from 'react'
+import { useRef, useState, useMemo, useCallback } from 'react'
 import { searchMovies } from '../services/movies.js'
 
 //const API_MOVIES_URL = `https://www.omdbapi.com/?apikey=34bda7af&S=$(inputSearch)`
 
-
-export function useMovies({ inputSearch}) {
+export function useMovies({ inputSearch, sort}) {
     const [movies, setMovies] = useState([])
     const [error, setError] = useState(null)
     const [loading, setLoanding] = useState(false)
     const previusSearch = useRef({ inputSearch })
     
 
-    
+    const getMovies = useCallback(
+        async ({ inputSearch }) => {
+      
+        if(inputSearch === previusSearch.current) return
 
-    const getMovies = async () => {
-      if(inputSearch === previusSearch.current) return
+        try{
+          setLoanding(true)
+          setError(null)
+          previusSearch.current = inputSearch   
+          const newMovies = await searchMovies({ inputSearch })
+          setMovies(newMovies)
 
-      try{
-        setLoanding(true)
-        setError(null)
-        previusSearch.current = inputSearch   
-        const newMovies = await searchMovies({ inputSearch })
-        setMovies(newMovies)
-
-      } catch (e) {
-          setError(e.message)
-      } finally {
-          setLoanding(false)
-
+        } catch (e) {
+            setError(e.message)
+        } finally {
+            setLoanding(false)
+        }
       }
-    }
+    ,[])
+
+    const sortedMovies = useMemo(()=>{ 
+      
+      return sort 
+      ? [ ...movies].sort((a, b) => a.title.localeCompare(b.title)) 
+      : movies
+    }, [sort, movies])
+      
     
-      return { movies, getMovies, loading}
+    return { movies: sortedMovies, getMovies, loading}
   }
